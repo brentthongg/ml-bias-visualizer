04bb61c85be5e02618be0af5be5abc2c07421ae1
Brent Hong <brent.hong99@gmail.com>
2021-05-07 23:59:50 -0400

    Comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
-function resizeWindowReset() {
+// Makes the intro div in the middle of the page upon resize
+function resizeWindowReset() { 
+    
     let divHeight = $('#main-container').height() / 2.0;
     let headerHeight = $("#top-bar").height();
     let windowHeight = $(window).height() / 2.0;
@@ -8,6 +10,7 @@ function resizeWindowReset() {
     $('#main-container').css('margin-bottom', `${windowHeight}px`);
 }
 
+// Checks if an element is within viewport.
 function elementWithinViewport(elem) {
     let elemTop = $(elem).offset().top;
     let elemBot = elemTop + $(elem).outerHeight();
@@ -26,6 +29,7 @@ $(document).ready(function() {
     // Animation upon scrolling learned from:
     // https://scotch.io/tutorials/level-up-your-websites-with-animatecss
     $(window).scroll(function () {
+        // Resets so that animation is played again when seen again
         $('.animate__animated').each(function() {
             if (!this.classList.contains('await-animation') && !elementWithinViewport(this)) {
                 $(this).addClass('await-animation');
@@ -33,7 +37,7 @@ $(document).ready(function() {
                 $(this).removeClass('animate__fadeInDown');
             }
         })
-
+        
         $('.await-animation').each(function() {
             if (elementWithinViewport(this)) {
                 $(this).addClass('animate__animated');
